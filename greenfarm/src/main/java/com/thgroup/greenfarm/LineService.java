ece41353a9ba447b1efed6f3f37503c10fcc7fdd
Cristian Voinicaru <cristian.voinicaru@thgroup.ch>
2024-08-05 14:19:05 +0200

    add last commit

--- greenfarm/src/main/java/com/thgroup/greenfarm/LineService.java
+++
@@ -1,8 +0,0 @@
-package com.thgroup.greenfarm;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-public interface LineService {
-    String sendLinePost() throws IOException, InterruptedException, URISyntaxException;
-}
--- greenfarm/src/main/java/com/thgroup/greenfarm/SMSService.java
+++
@@ -1,9 +0,0 @@
-package com.thgroup.greenfarm;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-public interface SMSService {
-    String sendSMSGet() throws IOException, InterruptedException, URISyntaxException;
-    String sendSMSPost() throws IOException, InterruptedException, URISyntaxException;
-}
